a10169cee356a4835066b5ee79ca832d14b655db
coderdreamer93 <dreamercoder93@gmail.com>
2024-08-11 22:44:33 +0500

    updated

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const cors = require("cors");
 const connectDB = require("./config/db")
 // import authRouter from './routes/authRoute.js';
 const authRouter  = require('./routes/authRoute')
+const inventoryRoute = require('./routes/inventory.route')
+const salesRoute  = require('./routes/sales.route')
+const purchaseRoute = require('./routes/purchase.route')
 
 dotenv.config();
 
@@ -23,12 +26,12 @@ app.use(cors());
 
 // Define routes
 app.use('/api/auth', authRouter);
-app.use("/api/v1/test", require("./routes/testRoutes"));
-app.use("/api/v1/sales", require("./routes/sales.route"))
-app.use("/api/v1/login", require("./routes/loginRoute"));
-app.use('/api/v1/inventories', require("./routes/inventory.route"));
-app.use('/api/v1/register', require("./routes/register.route"));
-app.use('/api/v1/purchase', require("./routes/purchase.route"));
+// app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/sales", salesRoute)
+// app.use("/api/v1/login", require("./routes/loginRoute"));
+app.use('/api/inventories', inventoryRoute);
+// app.use('/api/v1/register', require("./routes/register.route"));
+app.use('/api/purchase', purchaseRoute);
 
 
 // app.use('/api/v1/forms', require("./routes/formRoutes"));
