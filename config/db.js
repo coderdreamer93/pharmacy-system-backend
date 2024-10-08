3cd8463eba1519ff8713248d8fe8d026fcb9e387
coderdreamer93 <dreamercoder93@gmail.com>
2024-10-08 00:07:18 -0700

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const colors = require("colors");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect('mongodb+srv://nomannasir:lrrh0sUhTCs1ONyg@cluster0.jucwity.mongodb.net/PHARMACY_APPLICATION');
     console.log(`Connected to mongo db ${mongoose.connection.host}`);
   } catch (error) {
     console.log(`Mongo DB  Error ${error}`.bgRed.white);
